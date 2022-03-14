7da3837c366d4834d91f9042b28d37ab0537c42d
Zhenya <zhenia.zhak@gmail.com>
2022-03-14 20:59:31 +0300

    Added seed for products

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,20 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+product_1 = Product.create(name: 'Green Tea', code: 'GR1', price: 3.11)
+DiscountRule.create(
+  product: product_1,
+  discount_type: :buy_few_get_one,
+  discount_buildup: { paid_quantity: 1 }
+)
+
+product_2 = Product.create(name: 'Strawberries', code: 'SR1', price: 5.00)
+DiscountRule.create(
+  product: product_2,
+  discount_type: :price_discount,
+  discount_buildup: { from_quantity: 3, discount_price: 4.5 }
+)
+
+product_3 = Product.create(name: 'Coffee', code: 'CF1', price: 11.23)
+DiscountRule.create(
+  product: product_3,
+  discount_type: :price_ratio,
+  discount_buildup: { from_quantity: 3, multiplier: 2, divider: 3 }
+)
